0cfebfd3204248b8776d40292b68d08e45b990dd
vitordanielrosa <vitorcabral2004@gmail.com>
2026-05-31 17:22:50 -0300

    feat: update elevator API routes by removing redundant path and reordering for clarity

--- Projeto_iot/iot/urls.py
+++ Projeto_iot/iot/urls.py
@@ -3,14 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-
     path("entrega/criar/", views.criar_entrega, name="criar_entrega"),
 
     path("api/elevador/comando/", views.comando_arduino, name="comando_arduino"),
     path("api/elevador/status/", views.status_atual, name="status_atual"),
-    path("api/elevador/retornar/", views.retornar_terreo, name="retornar_terreo"),
-    path("api/elevador/finalizar/", views.finalizar_entrega, name="finalizar_entrega"),
     path("api/elevador/voltar-baixo/", views.voltar_para_baixo, name="voltar_para_baixo"),
+    path("api/elevador/finalizar/", views.finalizar_entrega, name="finalizar_entrega"),
 
     path(
         "api/elevador/status/<int:entrega_id>/<str:status>/",
