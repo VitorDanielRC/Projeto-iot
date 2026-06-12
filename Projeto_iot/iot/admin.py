f31a50e6c2d1205d24068078c3f78179b6804ac0
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-06-11 21:42:45 -0300

    admin

--- Projeto_iot/iot/admin.py
+++ Projeto_iot/iot/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Entrega
+
+
+@admin.register(Entrega)
+class EntregaAdmin(admin.ModelAdmin):
+    list_display = ("numero_pedido", "nome_morador", "andar_destino", "status", "executado", "criado_em")
+    list_filter = ("status", "andar_destino", "executado")
+    search_fields = ("numero_pedido", "nome_morador")
+    ordering = ("-criado_em",)
