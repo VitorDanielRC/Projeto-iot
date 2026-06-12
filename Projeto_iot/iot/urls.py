5353e0ebb6c56e9441c3e8584b515a4d1dc35eb3
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-06-11 21:43:14 -0300

    urls

--- Projeto_iot/iot/urls.py
+++ Projeto_iot/iot/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path("painel/", views.painel, name="painel"),
     path("logout/", views.logout_view, name="logout"),
 
+    path("entrega/status-atual/", views.status_painel, name="status_painel"),
     path("entrega/criar/", views.criar_entrega, name="criar_entrega"),
+    path("entrega/voltar-baixo/", views.voltar_para_baixo_site, name="voltar_para_baixo_site"),
+    path("entrega/finalizar/", views.finalizar_entrega_site, name="finalizar_entrega_site"),
 
     path("api/elevador/comando/", views.comando_elevador, name="comando_elevador"),
     path("api/elevador/status/", views.status_elevador, name="status_elevador"),
-    path("api/elevador/voltar-baixo/", views.voltar_para_baixo, name="voltar_para_baixo"),
-    path("api/elevador/finalizar/", views.finalizar_entrega, name="finalizar_entrega"),
+    path("api/elevador/voltar-baixo/", views.voltar_para_baixo_api, name="voltar_para_baixo_api"),
+    path("api/elevador/finalizar/", views.finalizar_entrega_api, name="finalizar_entrega_api"),
     path("api/elevador/status/<int:entrega_id>/<str:novo_status>/", views.atualizar_status, name="atualizar_status"),
 ]
