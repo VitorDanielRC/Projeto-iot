8b4a0e090b886bc28dd74fa6d13ac20de3751f9d
vitordanielrosa <vitorcabral2004@gmail.com>
2026-06-12 21:32:08 -0300

    Use fake-initial migrations on Render

--- Projeto_iot/start.sh
+++ Projeto_iot/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python manage.py migrate --no-input
+python manage.py migrate --fake-initial --no-input
 python manage.py create_admin_from_env
 python -m gunicorn Projeto_iot.wsgi:application
